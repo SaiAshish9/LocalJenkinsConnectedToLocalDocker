ba336a6aceef8513787a5028ff4ce244ccf02a61
SaiAshish <saiashish3760@gmail.com>
2023-01-31 23:53:41 +0530

    Update Steps.java

--- Steps.java
+++ Steps.java
@@ -1,5 +1,8 @@
 /*
 Create new item
-
-
+Select Freestyle project
+Provide github project link
+Generate access token at docker hub and use it as the password
+Make use of Build/Publish docker image steps
+Install docker plugins via available plugins section ( Docker, Docker Pipeline Docker Build Step and Cloud Bees Docker Build & Publish   )
 /*
